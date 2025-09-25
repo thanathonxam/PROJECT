d74cd6ac3542dd3bee9d24678191861f0f1372a3
thanathonxam <thanathon.ku@ku.th>
2025-09-25 14:23:52 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -46,7 +46,7 @@ public class Main extends JFrame {
             }
         });
 
-        // ปุ่ม Settings
+        // ปุ่ม Continue
         JButton settingsButton = new JButton("Continue");
         settingsButton.setFont(new Font("Arial", Font.BOLD, 30));
         settingsButton.setBackground(new Color(30, 144, 255)); // สีน้ำเงิน
@@ -85,3 +85,4 @@ public class Main extends JFrame {
         setVisible(true);
     }
 }
+
